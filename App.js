65d947545baf69b617670472be9b20696b40d4ee
Molly Treadwell <mhtreadwell718@gmail.com>
2023-04-24 17:59:48 -0400

    route edit

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import './App.css';
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Races from "./components/Races";
 import Community from "./components/Community";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
       <Route exact path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/contact" element={<Contact/>}/>
-      <Route path="/races" element={<Races/>}/>
       <Route path="/community" element={<Community/>}/>
       </Routes>
     </Router>
