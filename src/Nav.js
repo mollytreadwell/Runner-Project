15caf15981e51d0782be72dd70bd451a2e457f77
Molly Treadwell <mhtreadwell718@gmail.com>
2023-04-24 17:35:50 -0400

    edit package.json

--- src/Nav.js
+++ src/Nav.js
@@ -12,14 +12,10 @@ export default function Navpill(){
             <a className="nav-link" href="/About">About</a>
         </li>
         <li className="nav-item">
-<<<<<<< HEAD
-            <a className="nav-link" href="./Community">Community</a>
-=======
             <a className="nav-link" href="/Races">Race List</a>
         </li>
         <li className="nav-item">
             <a className="nav-link" href="/Community">Community</a>
->>>>>>> 07ea7060f1a68bb4566b347bff6520931afaeda6
         </li>
         <li className="nav-item">
             <a className="nav-link" href="/Contact">Contact</a>
