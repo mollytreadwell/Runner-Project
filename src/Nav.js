3ef57a520df5bc724e7ebc3fbce4c1b31479e365
Molly Treadwell <mhtreadwell718@gmail.com>
2023-04-24 17:46:47 -0400

    edit nav

--- src/Nav.js
+++ src/Nav.js
@@ -11,9 +11,7 @@ export default function Navpill(){
         <li className="nav-item">
             <a className="nav-link" href="/About">About</a>
         </li>
-        <li className="nav-item">
-            <a className="nav-link" href="/Races">Race List</a>
-        </li>
+
         <li className="nav-item">
             <a className="nav-link" href="/Community">Community</a>
         </li>
